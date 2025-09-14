249fde29fafd00bedc210f93ce108bec740920a2
Sanket Kumar Verma <sanketv403@gmail.com>
2025-09-14 16:21:55 +0530

    2nd commit

--- firstSpring/src/main/java/com/sanket/firstSpring/HelloController.java
+++ firstSpring/src/main/java/com/sanket/firstSpring/HelloController.java
@@ -1,6 +1,8 @@
 package com.sanket.firstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // This annotation indicates that this is a combination a ResponseBody & controller
@@ -10,4 +12,11 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+
+    @PostMapping("/hello")
+    public String postMethodName(@RequestBody String name) {
+
+        return "Hello " + name + "!";
+    }
+
 }
