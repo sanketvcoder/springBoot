28744c42b622a691b563a60601e1ea710bf2f563
Sanket Kumar Verma <sanketv403@gmail.com>
2025-09-14 19:27:54 +0530

    4th commit commit by sanket

--- FirstJobApp/src/main/java/com/sanket/FirstJobApp/controller/jobController.java
+++ FirstJobApp/src/main/java/com/sanket/FirstJobApp/controller/jobController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sanket.FirstJobApp.entity.Job;
@@ -18,4 +20,11 @@ public class jobController {
         // Logic to retrieve all jobs
         return jobList;
     }
+
+    @PostMapping("/jobs")
+    public String createJobs(@RequestBody Job entity) {
+        jobList.add(entity);
+        return "Job added successfully";
+    }
+
 }
